a675b1912994b324ae3aba20feccf9919ec3e564
sheffien <sheffien@users.noreply.github.com>
2015-04-27 16:32:39 +0200

    Correct logOdds col to logOddsRatio

--- R/calcLocEnrichment.R
+++ R/calcLocEnrichment.R
@@ -23,7 +23,7 @@
 calcLocEnrichment = function(userSets, userUniverse, regionDB, cores=1, redefineUserSets=FALSE) {
 	# Silence R CMD check Notes:
 	support=d=b=userSet=pValueLog=rnkSup=rnkPV=rnkLO=NULL
-	logOdds=maxRnk=meanRnk=dbSet=description=NULL
+	logOddsRatio=maxRnk=meanRnk=dbSet=description=NULL
 	annotationDT = regionDB$regionAnno
 	testSetsGRL = regionDB$regionGRL
 	annotationDT[, dbSet := 1:nrow(annotationDT)]
@@ -78,12 +78,12 @@ calcLocEnrichment = function(userSets, userUniverse, regionDB, cores=1, redefine
 		warning("Negative c entry in table. Bug with userSetsLength; this should not happen.");
 		return(scoreTable);
 	}
-	scoreTable[,c("pValueLog", "logOdds") := fisher.test(matrix(c(support,b,c,d), 2, 2), alternative='greater')[c("p.value", "estimate")], by=list(userSet,dbSet)]
+	scoreTable[,c("pValueLog", "logOddsRatio") := fisher.test(matrix(c(support,b,c,d), 2, 2), alternative='greater')[c("p.value", "estimate")], by=list(userSet,dbSet)]
 	scoreTable[, pValueLog:=-log(pValueLog)]
 	### Finalize and Rank results ###
 	scoreTable[, rnkSup:=rank(-support, ties.method="min"), by=userSet]
 	scoreTable[, rnkPV:=rank(-pValueLog, ties.method="min"), by=userSet]
-	scoreTable[, rnkLO:=rank(-logOdds, ties.method="min"), by=userSet]
+	scoreTable[, rnkLO:=rank(-logOddsRatio, ties.method="min"), by=userSet]
 	scoreTable[, maxRnk:=max(c(rnkSup, rnkPV, rnkLO)), by=list(userSet,dbSet)]
 	scoreTable[, meanRnk:=signif(mean(c(rnkSup, rnkPV, rnkLO)), 3), by=list(userSet,dbSet)]
 
@@ -94,7 +94,7 @@ calcLocEnrichment = function(userSets, userUniverse, regionDB, cores=1, redefine
 	# limit description to 80 characters
 	scoreTable[,description:=substr(description, 0, 80)]
 
-	orderedCols = c("userSet", "dbSet", "collection", "pValueLog", "logOdds", "support", "rnkPV", "rnkLO", "rnkSup", "maxRnk", "meanRnk", "b", "c", "d", "cellType", "tissue", "antibody", "treatment", "dataSource", "filename", "description")
+	orderedCols = c("userSet", "dbSet", "collection", "pValueLog", "logOddsRatio", "support", "rnkPV", "rnkLO", "rnkSup", "maxRnk", "meanRnk", "b", "c", "d", "cellType", "tissue", "antibody", "treatment", "dataSource", "filename", "description")
 	unorderedCols = setdiff(colnames(scoreTable), orderedCols)
 
 	setcolorder(scoreTable,  c(orderedCols, unorderedCols));
