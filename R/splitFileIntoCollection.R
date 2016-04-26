37c1afba388b9c7a7948d331b962155ba195c49e
sheffien <sheffien@users.noreply.github.com>
2016-04-26 16:53:37 -0700

    update splitFileIntoCollection parameters

--- R/splitFileIntoCollection.R
+++ R/splitFileIntoCollection.R
@@ -5,19 +5,27 @@
 #' and split it into separate files for use with the LOLA collection format.
 #'
 #' @param filename	the file to split
-#' @param splitCol	factor column that groups the lines in the file by set
+#' @param splitCol	factor column that groups the lines in the file by set. It 
+#' 	should be an integer.
+#' @param collectionFolder	name of folder to place the new split files.
+#' @param filenamePrepend	string to prepend to the filenames. Defaults to blank.
 #'
 #' @return No return value.
 #' @export
 #' @examples
 #' combFile = system.file("extdata", "examples/combined_regions.bed", package="LOLA")
 #' splitFileIntoCollection(combFile, 4)
-splitFileIntoCollection = function(filename, splitCol) {
+splitFileIntoCollection = function(filename, splitCol, collectionFolder=NULL,
+		filenamePrepend="") {
 	DT = fread(paste0(filename))
-	collectionFolder = paste0(dirname(filename), "/", basename(filename), "_collection")
+	if (is.null(collectionFolder)) {
+		# Default collection folder:
+		collectionFolder = paste0(dirname(filename), "/", basename(filename), "_collection")
+	}
 	dir.create(collectionFolder)
 	sDT = splitDataTable(DT, splitFactor=splitCol)
 	nDT = names(sDT)
+	nDT = paste0(filenamePrepend, nDT)
 	for (i in seq_along(sDT)) {
 		message(nDT[[i]])
 		write.tsv(sDT[[i]], paste0(collectionFolder, "/", nDT[[i]], ".bed"))
