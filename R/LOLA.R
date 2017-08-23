1d6deb9b87d3574ade1994573093251303fe7d83
nsheff <nathan@code.databio.org>
2017-08-22 20:54:12 -0400

    import reshape2 test

--- R/LOLA.R
+++ R/LOLA.R
@@ -9,7 +9,7 @@
 #'
 #' @references \url{http://github.com/sheffien}
 #' @importFrom GenomicRanges GRanges GRangesList elementMetadata strand
-#' @import BiocGenerics S4Vectors IRanges
+#' @import BiocGenerics S4Vectors IRanges reshape2
 #' @importFrom data.table ":=" setDT data.table setkey fread setnames as.data.table setcolorder melt setkeyv
 #' @importFrom stats fisher.test setNames
 #' @importFrom utils write.table
