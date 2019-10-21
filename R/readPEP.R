25f46eb6ce77af2007c514c800930fe39532c492
Jose <j.esiempreliga@hotmail.com>
2019-10-21 18:03:24 -0400

    add code to load data as GRanges list

--- R/readPEP.R
+++ R/readPEP.R
@@ -4,17 +4,27 @@
 ##############################################################################################
 
 
-# Write a function that allows LOLA to use pepr so that it can read PEP-formatted databases 
+# Helper function that allows LOLA to read PEP-formatted databases 
 
 
 readPEP = function(...){
   library(pepr)
-  config = file.path(...)
-  pep_database = pepr::Project(file = config)
-  return(pep_database)
+  config_loc = file.path(...)
+  if (file.exists(config_loc) & length(config_loc == 1)) {
+    pep_obj = pepr::Project(file = config_loc)
+    # include samples annotation and config.yaml in list output
+    samples_annotation = samples(pep_obj)
+    pep_config = config(pep_obj)
+    samples_df = as.data.frame(samples_annotation)
+    chrom_ranges = lapply(samples_df$file_path, fread) # need to make list into a granges list object
+    regions = GRangesList(chrom_ranges)
+    return(nlist(config_loc, samples_annotation, pep_config, regions))
+    
+  } else {
+    stop("could not find .yaml config file")
+    }
 }
 
 #pep_object = readPEP("/project","shefflab","resources","regions","LOLAHema","PEP_project","project_config.yaml")
 
-#samples(pep_object)
-#config(pep_object)
+
