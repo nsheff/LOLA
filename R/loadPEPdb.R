70ee370d19309e53939f40d98c5de3c1f1f02eb4
Jose <j.esiempreliga@hotmail.com>
2019-10-22 12:34:33 -0400

    use readBED instead of fread

--- R/loadPEPdb.R
+++ R/loadPEPdb.R
@@ -8,21 +8,20 @@
 #' @param config_location folder where the project_config.yaml file is located 
 
 
-readPEP = function(...){
-  library(pepr)
-  config_loc = file.path(...)
-  if (file.exists(config_loc) & length(config_loc == 1)) {
+loadPEPdb = function(...){
+  config_loc = file.path(...) # feed config location into Project function 
+  if (file.exists(config_loc)) {
     pep_obj = pepr::Project(file = config_loc)
     # include samples annotation and config.yaml in list output
     samples_annotation = samples(pep_obj)
-    pep_config = config(pep_obj)
+    config_file = config(pep_obj)
     samples_df = as.data.frame(samples_annotation)
     chrom_ranges = lapply(samples_df$file_path, readBed) # need to make list into a granges list object
     regions = GRangesList(chrom_ranges)
-    return(list(config_loc, samples_annotation, pep_config, regions))
+    return(list(config_loc, samples_annotation,config_file, regions))
     
   } else {
-    stop("could not find .yaml config file")
+    stop("could not find .yaml config file :(")
     }
 }
 
