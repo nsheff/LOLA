ca4499404692cfaf9e0bb7400439b59bc4985133
Jose <j.esiempreliga@hotmail.com>
2019-10-23 15:26:11 -0400

    update description

--- R/loadPEPdb.R
+++ R/loadPEPdb.R
@@ -5,18 +5,22 @@
 
 
 #' Helper function that allows LOLA to read PEP-formatted databases 
-#' @param config_location folder where the project_config.yaml file is located 
+#' 
+#' @param config_location folder where the project_config.yaml file is located
+#' @return PEPdB contains database location, samples annotation sheet, .yaml config file 
+#' and the regions Granges list 
 
 
 loadPEPdb = function(config_location){
-  config_loc = file.path(path=config_location) # feed config location into Project function 
+  library(pepr)
+  config_loc = file.path(path=config_location)  
   if (file.exists(config_loc)) {
       pep_obj = pepr::Project(file = config_loc)
       # include samples annotation and config.yaml in list output
       samples_annotation = samples(pep_obj)
       config_file = config(pep_obj)
       samples_df = as.data.frame(samples_annotation)
-      chrom_ranges = lapply(samples_df$file_path, readBed) # need to make list into a granges list object
+      chrom_ranges = lapply(samples_df$file_path, readBed) # need to make list into a GRanges list object
       regions = GRangesList(chrom_ranges)
       return(list(config_loc, samples_annotation,config_file, regions))
   } else {
