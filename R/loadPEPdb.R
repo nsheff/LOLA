127efbb6b273a526d64dd10a3a71460012288653
Jose <j.esiempreliga@hotmail.com>
2019-10-24 17:21:55 -0400

    add export and example

--- R/loadPEPdb.R
+++ R/loadPEPdb.R
@@ -9,25 +9,34 @@
 #' @param config_location folder where the project_config.yaml file is located
 #' @return PEPdB contains database location, samples annotation sheet, .yaml config file 
 #' and the regions Granges list 
+#' @export
+#' @examples 
+#' dbPath = system.file("extdata", "hg19", package="LOLA")
+#' PEPdb = loadPEPdb(configLocation=dbpath)
+#' 
 
 
-loadPEPdb = function(config_location){
-  library(pepr)
-  config_loc = file.path(path=config_location)  
-  if (file.exists(config_loc)) {
-      pep_obj = pepr::Project(file = config_loc)
+loadPEPdb = function(configLocation){
+  configLoc = file.path(path=configLocation)  
+  if (file.exists(configLoc)) {
+      pep_obj = pepr::Project(file = configLoc)
       # include samples annotation and config.yaml in list output
-      samples_annotation = samples(pep_obj)
-      config_file = config(pep_obj)
-      samples_df = as.data.frame(samples_annotation)
-      chrom_ranges = lapply(samples_df$file_path, readBed) # need to make list into a GRanges list object
-      regions = GRangesList(chrom_ranges)
-      return(list(dblocation = config_loc, regionAnno = samples_annotation, regionGRL = regions))
+      samplesAnnotation = pepr::samples(pep_obj)
+      configFile = pepr::config(pep_obj)
+      samplesdf = as.data.frame(samplesAnnotation)
+      chromRanges = lapply(samplesdf$file_path, LOLA::readBed) # need to make list into a GRanges list object
+      regions = GRangesList(chromRanges)
+      return(list(dblocation = configLoc,
+                  configYAML = configFile,
+                  regionAnno = samplesAnnotation[, -c("file_path", "genome")],
+                  regionGRL = regions))
   } else {
       stop("could not find .yaml config file")
   }
 }
 
-pep_object = loadPEPdb("/project/shefflab/resources/regions/LOLAHema/LOLAHema_PEP/project_config.yaml")
+#pep_object = loadPEPdb("/project/shefflab/resources/regions/LOLAHema/LOLAHema_PEP/project_config.yaml")
+
+
 
 
