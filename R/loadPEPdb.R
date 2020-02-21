b6600a3ed0a76d1ab37bd0076ff0cf6f6a55580b
joseverdezoto <j.esiempreliga@hotmail.com>
2020-02-21 12:24:38 -0500

    fix code style issues, update example config and annotation

--- R/loadPEPdb.R
+++ R/loadPEPdb.R
@@ -1,56 +1,59 @@
-############################################################################################## 
+############################################################################
 # PEP-loading function 
 
-##############################################################################################
+############################################################################
 
 
-#' Helper function that allows LOLA to read PEP-formatted databases 
-#' 
-#' @param configFolder     folder where the config.yaml file is located
-#' @param configName       Name of the config.yaml file 
-#' @param useCache            uses simpleCache to cache and load the results    
-#' @return PEPdB contains database location, samples annotation sheet, .yaml config file 
-#' and the regions Granges list 
+#' Given a PEP-formatted database, this function will display its metadata and 
+#' load the genomic regions for runLOLA() overlap calculations
+#' @param configPath Psth to the PEP config.yaml file  
+#' @param useCache   uses simpleCache to cache and load the results    
+#' @return A list containing the config location, config file,   
+#' samples annotation sheet and the regions Granges list 
 #' @export
 #' @examples 
-#' ConfigPath = system.file("extdata", "hg19", "ucsc_examplePEP", "project_config.yaml", package="LOLA")
-#' PEPdb = loadPEPdb_CACHE(configLocation=ConfigPath)
+#' ConfigPath = system.file("extdata/hg19/ucsc_examplePEP/project_config.yaml",
+#'                         package="LOLA")
+#' PEPdb = loadPEPdb(ConfigPath)
 #' 
 
 
-loadPEPdb = function(configFolder, configName, useCache=TRUE){
-	configLoc = file.path(path=paste0(configFolder, configName))
-	if (file.exists(configLoc)) {
+loadPEPdb = function(configPath, useCache=TRUE){
+	configLoc = file.path(path=configPath)
+	if (!file.exists(configLoc)) {
+		stop("Could not find .yaml config file")
+	} else {
 		# Use pepr to read in the PEP metadata
 		pepObject = pepr::Project(file = configLoc)
 		configFile = pepr::config(pepObject)
-		samplesAnnotation = pepr::samples(pepObject)
+		smpl = pepr::samples(pepObject)
 		# Rename columns to make annotation LOLA compatible 
-		colnames(samplesAnnotation)[colnames(samplesAnnotation)=="file_name"] = "filename"
-		colnames(samplesAnnotation)[colnames(samplesAnnotation)=="cell_type"] = "cellType"
-		colnames(samplesAnnotation)[colnames(samplesAnnotation)=="exp_protocol"] = "collection"
-		colnames(samplesAnnotation)[colnames(samplesAnnotation)=="data_source"] = "dataSource"
-		# Output the samples annotation and make it a dataframe so that we can lapply through each file path
-		samplesdf = as.data.frame(samplesAnnotation)
+		colnames(smpl)[colnames(smpl)=="file_name"] = "filename"
+		colnames(smpl)[colnames(smpl)=="cell_type"] = "cellType"
+		colnames(smpl)[colnames(smpl)=="exp_protocol"] = "collection"
+		colnames(smpl)[colnames(smpl)=="data_source"] = "dataSource"
+		# Make samples annotation a df and lapply through each file path
+		samplesdf = as.data.frame(smpl)
+		configFolder = dirname(configPath)
 		if (useCache & requireNamespace("simpleCache", quietly=TRUE)){
-			simpleCache::simpleCache("chromRanges", { # need to make regions GRanges objects and cache the data
-				lapply(samplesdf$output_file_path, LOLA::readBed)}, 
-								cacheDir=file.path(path=configFolder),
-								recreate=FALSE) 
+			simpleCache::simpleCache("chromRanges", { 
+			lapply(samplesdf$output_file_path, LOLA::readBed)}, 
+			cacheDir=file.path(path=configFolder), 
+			recreate=FALSE) 
 		} else {
 			if (nrow(samplesAnnotation) > 100) {
 				# tell the user they should install simpleCache 
-				message("You should install simpleCache in order to save time when loading your database next")
-		}
+				message("You should install simpleCache 
+					to save time when you load the 
+					database next")
+			}
 			chromRanges = lapply(samplesdf$file_path, LOLA::readBed)    
-	}
-	regions = GRangesList(chromRanges)
-	return(list(configLocation = configLoc,
+		}
+		regions = GRangesList(chromRanges)
+		return(list(configLocation = configLoc,
 				configYAML = configFile,
-				regionAnno = samplesAnnotation[, -c("genome")],
+				regionAnno = smpl[, -c("genome")],
 				regionGRL = regions))
-	} else {
-		stop("could not find .yaml config file")
 	}
 }
 
